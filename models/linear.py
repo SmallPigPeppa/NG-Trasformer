7ba0ee68a64a7598211cef9e5111cb6ce6c32e4d
lwz <905328134@qq.com>
2024-05-26 01:03:43 +0800

    initial

--- models/linear.py
+++ models/linear.py
@@ -7,17 +7,19 @@ from torchvision.models import resnet50
 
 
 class MLP(pl.LightningModule):
-    def __init__(self, dim_feature, num_classes, pl_lambda, lr, epochs, warmup_epochs, **kwargs):
+    def __init__(self, dim_feature, num_classes, lr, epochs, warmup_epochs, **kwargs):
         super(MLP, self).__init__()
         self.dim_feature = dim_feature
         self.num_calsses = num_classes
-        self.pl_lambda = pl_lambda
         self.lr = lr
         self.epochs = epochs
         self.warmup_epochs = warmup_epochs
         self.extra_args = kwargs
         self.encoder = None
         self.fc = nn.Linear(dim_feature, num_classes)
+
+    def on_train_epoch_start(self):
+        self.encoder.eval()
 
     def configure_optimizers(self):
         optimizer = torch.optim.SGD(self.parameters(), lr=self.lr)
@@ -58,4 +60,3 @@ class MLP(pl.LightningModule):
         log_dict = {"test/" + k: v for k, v in out.items()}
         self.log_dict(log_dict, on_epoch=True, sync_dist=True)
         return out
-

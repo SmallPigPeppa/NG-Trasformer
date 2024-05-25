6a3b905d3b3aa0888b5169875c268806a66e554e
lwz <905328134@qq.com>
2024-05-26 00:38:34 +0800

    initial

--- train.py
+++ train.py
@@ -21,58 +21,31 @@ def main():
         encoder = get_pretrained_encoder(args.pretrained_model, cifar=False)
 
     model = MLP(**args.__dict__)
-
-    classes_order = torch.tensor(
-        [68, 56, 78, 8, 23, 84, 90, 65, 74, 76, 40, 89, 3, 92, 55, 9, 26, 80, 43, 38, 58, 70, 77, 1, 85, 19, 17, 50, 28,
-         53, 13, 81, 45, 82, 6, 59, 83, 16, 15, 44, 91, 41, 72, 60, 79, 52, 20, 10, 31, 54, 37, 95, 14, 71, 96, 98, 97,
-         2,64, 66, 42, 22, 35, 86, 24, 34, 87, 21, 99, 0, 88, 27, 18, 94, 11, 12, 47, 25, 30, 46, 62, 69, 36, 61, 7, 63,
-         75,5, 32, 4, 51, 48, 73, 93, 39, 67, 29, 49, 57, 33])
-    tasks_initial = classes_order[:int(args.num_classes / 2)].chunk(1)
-    tasks_incremental = classes_order[int(args.num_classes / 2):args.num_classes].chunk(args.num_tasks)
-    tasks = tasks_initial + tasks_incremental
     train_dataset, test_dataset = get_dataset(dataset=args.dataset, data_path=args.data_path)
 
-    for task_idx in range(0, args.num_tasks + 1):
-        wandb_logger = WandbLogger(
-            name=f"{args.dataset}-{args.pretrained_method}-{args.num_tasks}tasks-steps{task_idx}",
-            project=args.project,
-            entity=args.entity,
-            offline=False,
-        )
-        if args == 0:
-            wandb_logger.log_hyperparams(args)
-        lr_monitor = LearningRateMonitor(logging_interval="epoch")
-        train_dataset_task = split_dataset(
-            train_dataset,
-            tasks=tasks,
-            task_idx=list(range(task_idx + 1)),
-        )
-        test_dataset_task = split_dataset(
-            test_dataset,
-            tasks=tasks,
-            task_idx=list(range(task_idx + 1)),
-        )
-        train_dataset_task, test_dataset_task, cpn_means = get_pretrained_dataset(
-            encoder=encoder,
-            train_dataset=train_dataset_task,
-            test_dataset=test_dataset_task,
-            return_means=True)
-        train_loader = DataLoader(train_dataset_task, batch_size=64, shuffle=True)
-        test_loader = DataLoader(test_dataset_task, batch_size=64, shuffle=True)
-        trainer = pl.Trainer(
-            gpus=num_gpus,
-            max_epochs=args.epochs,
-            accumulate_grad_batches=1,
-            sync_batchnorm=True,
-            logger=wandb_logger,
-            enable_checkpointing=False,
-            precision=16,
-            callbacks=[lr_monitor]
+    wandb_logger = WandbLogger(
+        name=f"{args.dataset}-{args.pretrained_method}",
+        project=args.project,
+        entity=args.entity,
+        offline=False,
+    )
+    lr_monitor = LearningRateMonitor(logging_interval="epoch")
+    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=64, shuffle=True)
+    trainer = pl.Trainer(
+        gpus=num_gpus,
+        max_epochs=args.epochs,
+        accumulate_grad_batches=1,
+        sync_batchnorm=True,
+        logger=wandb_logger,
+        enable_checkpointing=False,
+        precision=16,
+        callbacks=[lr_monitor]
 
-        )
-        trainer.fit(model, train_loader, test_loader)
-        trainer.test(model, test_loader)
-        wandb.finish()
+    )
+    trainer.fit(model, train_loader, test_loader)
+    trainer.test(model, test_loader)
+    wandb.finish()
 
 
 if __name__ == '__main__':
--- utils/args_utils.py
+++ utils/args_utils.py
@@ -14,35 +14,21 @@ def parse_args_cpn() -> argparse.Namespace:
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--pretrained_model", type=str,
-                        required=True)
-    parser.add_argument("--pretrained_method", type=str,
-                        required=True)
-    # incremental
-    parser.add_argument("--num_tasks", type=int, default=5)
-    # cpn
-    parser.add_argument("--pl_lambda", type=float, default=0.2)
-    parser.add_argument("--lambda1", type=float, default=0.1)
-    parser.add_argument("--lambda2", type=float, default=0.1)
-    parser.add_argument("--dim_feature", type=int, default=2048)
-    parser.add_argument("--tau", type=float, default=1.0)
-
-
-    parser.add_argument("--cpn_initial", type=str, choices=["random", "means"], default="random")
+    parser.add_argument("--project", type=str, default="NG-Transformer")
+    parser.add_argument("--entity", type=str, default="pigpeppa")
+    parser.add_argument("--method", type=str, default="")
 
     parser.add_argument("--dataset", type=str, choices=["cifar100", "imagenet100","imagenet-subset"], default="cifar100")
-    parser.add_argument("--data_path", type=str, default="/share/wenzhuoliu/torch_ds")
-    parser.add_argument("--num_classes", type=int, default=100)
-
-    parser.add_argument("--epochs", type=int, default=300)
-
+    parser.add_argument("--data_path", type=str, default="/ppio_net0/torch_ds")
+    parser.add_argument("--epochs", type=int, default=30)
     parser.add_argument("--warmup_epochs", type=int, default=10)
     parser.add_argument("--batch_size", type=int, default=64)
     parser.add_argument("--lr", type=float, default=0.3)
 
-    parser.add_argument("--project", type=str, default="Incremental-CPN-v8.0")
-    parser.add_argument("--entity", type=str, default="pigpeppa")
-    parser.add_argument("--perfix", type=str, default="")
+    parser.add_argument("--num_classes", type=int, default=100)
+    parser.add_argument("--dim_feature", type=int, default=2048)
+    parser.add_argument("--tau", type=float, default=1.0)
+    parser.add_argument("--pretrained_patch", type=str, required=True)
 
     # parse args
     args = parser.parse_args()

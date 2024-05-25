f5e85647aa035fb692b1b2564c444cc972aba986
lwz <905328134@qq.com>
2024-05-26 01:31:12 +0800

    initial

--- models/linear.py
+++ models/linear.py
@@ -35,7 +35,7 @@ class MLP(pl.LightningModule):
         encoder.fc = nn.Identity()
         encoder.load_state_dict(state, strict=False)
         print(f"Loaded {self.extra_args['pretrain_ckpt']}")
-        self.encoder = encoder.eval()
+        # self.encoder = encoder.eval()
 
     def configure_optimizers(self):
         optimizer = torch.optim.SGD(self.parameters(), lr=self.lr)

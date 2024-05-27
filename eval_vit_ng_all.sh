1e57cbc32feb0a5f3abdbf069f52b6f58052479d
lwz <905328134@qq.com>
2024-05-27 23:27:51 +0800

    initial

--- eval_vit_ng_all.sh
+++ eval_vit_ng_all.sh
@@ -1,25 +1,29 @@
-import os
+#!/bin/bash
 
-# Define the range of keep ratios you want to evaluate
-keep_ratios = [1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1]
+PRETRAIN_CKPT="/ppio_net0/pretrained/byol-imagenet32-t3pmk238-ep=999.ckpt"
+DATA_PATH="/ppio_net0/torch_ds/imagenet"
+DATASET="imagenet"
+EPOCHS=30
+LR=0.1
+BATCH_SIZE=256
+NUM_GPUS=1
+NUM_WORKERS=8
+PROJECT="NG-Transformer"
 
-# Common command template
-command_template = (
-    "python eval_vit_ng.py "
-    "--pretrain_ckpt /ppio_net0/pretrained/byol-imagenet32-t3pmk238-ep=999.ckpt "
-    "--data_path /ppio_net0/torch_ds/imagenet "
-    "--dataset imagenet "
-    "--epochs 30 "
-    "--lr 0.1 "
-    "--batch_size 256 "
-    "--num_gpus 1 "
-    "--num_workers 8 "
-    "--project NG-Transformer "
-    "--run_name ViT-NG-ratio{ratio} "
-    "--keep_ratio {ratio}"
-)
-
-# Iterate through the defined keep ratios and run the command for each
-for ratio in keep_ratios:
-    command = command_template.format(ratio=ratio)
-    os.system(command)
+for RATIO in 1.0 0.9 0.8 0.7 0.6 0.5 0.4 0.3 0.2 0.1
+do
+  RUN_NAME="ViT-NG-ratio$RATIO"
+  echo "Running evaluation with --keep_ratio $RATIO"
+  python eval_vit_ng.py \
+      --pretrain_ckpt $PRETRAIN_CKPT \
+      --data_path $DATA_PATH \
+      --dataset $DATASET \
+      --epochs $EPOCHS \
+      --lr $LR \
+      --batch_size $BATCH_SIZE \
+      --num_gpus $NUM_GPUS \
+      --num_workers $NUM_WORKERS \
+      --project $PROJECT \
+      --run_name $RUN_NAME \
+      --keep_ratio $RATIO
+done

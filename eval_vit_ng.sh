71e5a05027a863d9c4bfd6f0aaed0c62e10569d0
lwz <905328134@qq.com>
2024-05-27 22:09:18 +0800

    initial

---
+++ eval_vit_ng.sh
@@ -0,0 +1,11 @@
+python train_linear_ng.py \
+      --pretrain_ckpt /ppio_net0/pretrained/byol-imagenet32-t3pmk238-ep=999.ckpt \
+      --data_path /ppio_net0/torch_ds \
+      --dataset imagenet100 \
+      --epochs 30 \
+      --lr 0.1 \
+      --batch_size 256 \
+      --num_gpus 1 \
+      --num_workers 8 \
+      --project NG-Transformer \
+      --run_name ViT-NG
